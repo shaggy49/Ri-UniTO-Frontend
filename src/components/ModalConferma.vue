27d7056ccc156265a5ad413d9e911bea7098ef7b
Alessandro Andro <pasandro.pasandro@gmail.com>
2022-01-08 23:15:33 +0100

    fix: fixed buttons not working
    buttons and inside text implemented with props

--- src/components/ModalConferma.vue
+++ src/components/ModalConferma.vue
@@ -3,28 +3,30 @@
         <div class="modal-background"></div>
         <div class="modal-content">
                 <div class="box has-text-centered mb-6 pt-5 pb-5 ">
-                    <h1 class="title pt-5">Prenotazione</h1>
+                    <h1 class="title pt-5">{{payload.actionTitle}}</h1>
                     <h2 class="subtitle pb-5">
-                        Confermi di volerti iscrivere?
+                        Confermi di volerti {{payload.actionText}}?
                     </h2>
-                    <h3 class="title is-3 pt-5">{{Materia}}</h3>
+                    <h3 class="title is-3 pt-3">{{payload.materia.materia}}</h3>
                     <p>
-                        {{Docente}}
+                        {{payload.materia.docente}}
                     </p>
                     <p >
-                        {{Data}}
+                        {{payload.materia.orario}}
                     </p>
-                        <button class="button mt-4 is-primary is-fullwidth">Conferma</button>
-                        <button class="button mt-4 is-fullwidth">
+                        <button @click="confirmAction" class="button mt-4 is-primary is-fullwidth">Conferma</button>
+                        <button @click="cancelAction" class="button mt-4 is-fullwidth">
                             Annulla
                         </button>
                 </div>
         </div>
-        <button class="modal-close is-large" aria-label="close"></button>
+        <button class="modal-close is-large" aria-label="close" @click="cancelAction"></button>
     </div>  
- </template>
+</template>
 <script>
 export default {
-    name: "ModalConferma"
+    name: "ModalConferma",
+    props:['payload', 'confirmAction', 'cancelAction'],
+
 }
 </script>
